5ae2a4b9073e635f02c73a22353b3a03b8b66c84
bmatsuo-at-luthersystems <bryan.matsuo@luthersystems.com>
2020-01-03 15:48:32 -0800

    ACRE-864:  aws-ecr-repository module to simplify ecr policy configuration

---
+++ aws-ecr-repository/policy.tf
@@ -0,0 +1,86 @@
+locals {
+  ecr_actions_ro = [
+    "ecr:GetDownloadUrlForLayer",
+    "ecr:BatchGetImage",
+    "ecr:BatchCheckLayerAvailability",
+    "ecr:DescribeRepositories",
+    "ecr:DescribeImages",
+    "ecr:ListImages",
+  ]
+
+  ecr_actions_rw = [
+    "ecr:GetDownloadUrlForLayer",
+    "ecr:BatchGetImage",
+    "ecr:BatchCheckLayerAvailability",
+    "ecr:PutImage",
+    "ecr:InitiateLayerUpload",
+    "ecr:UploadLayerPart",
+    "ecr:CompleteLayerUpload",
+    "ecr:DescribeRepositories",
+    "ecr:DescribeImages",
+    "ecr:ListImages",
+    "ecr:BatchDeleteImage",
+  ]
+
+  # NOTE:  If both ro_principals and rw_principals are empty then the following
+  # is not a valid policy document because it has an empty list of principals but
+  # that is OK because in that case no policy will be attached to the repository.
+  policy = "${
+    length(var.ro_principals) == 0 ? data.aws_iam_policy_document.rw.json :
+    length(var.rw_principals) == 0 ? data.aws_iam_policy_document.ro.json :
+                                     data.aws_iam_policy_document.rorw.json
+  }"
+}
+
+# policy that has only readonly principals
+data "aws_iam_policy_document" "ro" {
+  statement {
+    sid     = "readonlyAccess"
+    effect  = "Allow"
+    actions = ["${local.ecr_actions_ro}"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["${var.ro_principals}"]
+    }
+  }
+}
+
+# policy that has only readwrite principals
+data "aws_iam_policy_document" "rw" {
+  statement {
+    sid     = "readwriteAccess"
+    effect  = "Allow"
+    actions = ["${local.ecr_actions_rw}"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["${var.rw_principals}"]
+    }
+  }
+}
+
+# policy that has both readonly and readwrite principals
+data "aws_iam_policy_document" "rorw" {
+  statement {
+    sid     = "readonlyAccess"
+    effect  = "Allow"
+    actions = ["${local.ecr_actions_ro}"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["${var.ro_principals}"]
+    }
+  }
+
+  statement {
+    sid     = "readwriteAccess"
+    effect  = "Allow"
+    actions = ["${local.ecr_actions_rw}"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["${var.rw_principals}"]
+    }
+  }
+}
---
+++ aws-ecr-repository/vars.tf
@@ -0,0 +1,21 @@
+variable "name" {
+  type = "string"
+}
+
+# ro_principals lists AWS principals that have readonly access to the
+# repository (docker pull)
+#
+# ECR repository policies may only have principals which are roles, account
+# account roots, or users.
+variable "ro_principals" {
+  type = "list"
+}
+
+# rw_principals lists AWS principals that have readwrite access to the
+# repository (docker push & docker pull)
+#
+# ECR repository policies may only have principals which are roles, account
+# account roots, or users.
+variable "rw_principals" {
+  type = "list"
+}
