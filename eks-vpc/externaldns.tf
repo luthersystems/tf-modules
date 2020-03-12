70fc19e97e7807fa92ff223b7cc6b8d20e3de71e
bmatsuo-at-luthersystems <bryan.matsuo@luthersystems.com>
2020-03-11 17:16:53 -0700

    Roles for two external-dns deployments: external-dns-public and external-dns-private

--- eks-vpc/externaldns.tf
+++ eks-vpc/externaldns.tf
@@ -1,4 +1,4 @@
-module "externaldns_service_account_iam_role" {
+module "externaldns_public_service_account_iam_role" {
   source = "../eks-service-account-iam-role"
 
   luther_project = "${var.luther_project}"
@@ -8,25 +8,53 @@ module "externaldns_service_account_iam_role" {
 
   oidc_provider_name = "${local.oidc_provider_name}"
   oidc_provider_arn  = "${local.oidc_provider_arn}"
-  service_account    = "external-dns"
+  service_account    = "external-dns-public"
   k8s_namespace      = "external-dns"
   policy             = "${data.aws_iam_policy_document.externaldns.json}"
 
   providers = {
-    aws            = "aws"
-    template       = "template"
+    aws      = "aws"
+    template = "template"
   }
 }
 
-output "externaldns_service_account_role_arn" {
-  value = "${module.externaldns_service_account_iam_role.arn}"
+output "externaldns_public_service_account_role_arn" {
+  value = "${module.externaldns_public_service_account_iam_role.arn}"
+}
+
+module "externaldns_private_service_account_iam_role" {
+  source = "../eks-service-account-iam-role"
+
+  luther_project = "${var.luther_project}"
+  aws_region     = "${var.aws_region}"
+  luther_env     = "${var.luther_env}"
+  component      = "${var.component}"
+
+  oidc_provider_name = "${local.oidc_provider_name}"
+  oidc_provider_arn  = "${local.oidc_provider_arn}"
+  service_account    = "external-dns-private"
+  k8s_namespace      = "external-dns"
+  policy             = "${data.aws_iam_policy_document.externaldns.json}"
+
+  providers = {
+    aws      = "aws"
+    template = "template"
+  }
+}
+
+output "externaldns_private_service_account_role_arn" {
+  value = "${module.externaldns_private_service_account_iam_role.arn}"
 }
 
 # See the externaldns docs regarding its required IAM policy:
 #   https://github.com/kubernetes-sigs/external-dns/blob/master/docs/tutorials/aws.md
 data "aws_iam_policy_document" "externaldns" {
   statement {
-    actions   = ["route53:ChangeResourceRecordSets"]
+    actions = [
+      "route53:ChangeResourceRecordSets",
+      "route53:ListTagsForResource",
+    ]
+
     resources = ["arn:aws:route53:::hostedzone/*"]
   }
 
