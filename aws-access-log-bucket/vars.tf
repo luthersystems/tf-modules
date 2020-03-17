71227452c9dec59efdd9e5abd60b0d3464ce4ddb
Jeremy Asher <jeremy.asher@luthersystems.com>
2020-03-17 12:04:35 -0700

    clean up aws-access-log-bucket
    This:
    - updates the bucket policy to the latest recommended
    - removes the default value for a non-existent KMS key
    - replaces templating with locals
    - deletes unused code
    - removes the `expired_object_delete_marker` setting because that is not
      compatible with setting `days` on expiration and prevents plans from
      converging:
      https://docs.aws.amazon.com/AmazonS3/latest/dev/intro-lifecycle-rules.html
      https://github.com/terraform-providers/terraform-provider-aws/issues/9314

--- aws-access-log-bucket/vars.tf
+++ aws-access-log-bucket/vars.tf
@@ -19,8 +19,4 @@ variable "random_identifier" {
 
 variable "aws_kms_key_arn" {
   type = string
-
-  # This default is the Luther Systems default KMS key for ALB Access Log
-  # Buckets defined in the common-infrastructure repository.
-  default = "arn:aws:kms:eu-west-2:967058059066:key/3e991639-ebd3-4a1d-8bff-cb2e4555f5cd"
 }
