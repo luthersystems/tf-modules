4e45c314df79e7e7353deeedb4521eea0d27e5bc
Sam Wood <sam.wood@luthersystems.com>
2020-11-20 21:19:11 -0800

    Add storage account id output

--- az-blob-bucket/output.tf
+++ az-blob-bucket/output.tf
@@ -2,6 +2,10 @@ output "storage_account" {
   value = azurerm_storage_account.bucket.name
 }
 
+output "storage_account_id" {
+  value = azurerm_storage_account.bucket.id
+}
+
 output "storage_container" {
   value = azurerm_storage_container.bucket.name
 }
