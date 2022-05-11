7373ee55e11b6829e83944a74a3abbc979054cbc
Jeremy Asher <jeremy.asher@luthersystems.com>
2022-05-11 15:32:30 -0700

    provider upgrades part 1
    This removes provider constraints and resolves all validation errors with
    running the latest providers.

--- aws-cf-reverse-proxy/main.tf
+++ aws-cf-reverse-proxy/main.tf
@@ -22,17 +22,26 @@ resource "aws_acm_certificate" "site" {
 }
 
 resource "aws_route53_record" "site_validation" {
-  name    = aws_acm_certificate.site.domain_validation_options.0.resource_record_name
-  type    = aws_acm_certificate.site.domain_validation_options.0.resource_record_type
-  zone_id = data.aws_route53_zone.site.zone_id
-  records = [aws_acm_certificate.site.domain_validation_options.0.resource_record_value]
-  ttl     = 60
+  for_each = {
+    for dvo in aws_acm_certificate.site.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.site.zone_id
 }
 
 resource "aws_acm_certificate_validation" "site" {
   provider                = aws.us-east-1
   certificate_arn         = aws_acm_certificate.site.arn
-  validation_record_fqdns = [aws_route53_record.site_validation.fqdn]
+  validation_record_fqdns = [for record in aws_route53_record.site_validation : record.fqdn]
 }
 
 resource "aws_route53_record" "site" {
