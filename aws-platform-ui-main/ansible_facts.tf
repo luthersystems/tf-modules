703e8ee109a0d241aaa229f2400cf9bf755f1f13
Sam Wood <sam.wood@luthersystems.com>
2022-12-28 08:48:33 -0800

    Add managed prom and graf

--- aws-platform-ui-main/ansible_facts.tf
+++ aws-platform-ui-main/ansible_facts.tf
@@ -35,6 +35,9 @@ locals {
 
     externaldns_public_service_account_iam_role_arn  = module.eks_vpc.externaldns_facts.public_service_account_iam_role_arn
     externaldns_private_service_account_iam_role_arn = module.eks_vpc.externaldns_facts.private_service_account_iam_role_arn
+
+    prometheus_service_account_iam_role_arn = module.prometheus_service_account_iam_role.arn
+    prometheus_workspace_id                 = aws_prometheus_workspace.k8s.id
   }
 }
 
--- aws-platform-ui-main/aws.tf
+++ aws-platform-ui-main/aws.tf
@@ -2,6 +2,8 @@ data "aws_caller_identity" "current" {}
 
 data "aws_region" "current" {}
 
+data "aws_partition" "current" {}
+
 locals {
   account_id = data.aws_caller_identity.current.account_id
   region     = data.aws_region.current.name
