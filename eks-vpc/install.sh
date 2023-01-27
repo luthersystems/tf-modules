ed0cff006d3cfa1e46885dcfae1c1083fe636b44
Sam Wood <sam.wood@luthersystems.com>
2023-01-26 16:10:06 -0800

    Remove extra shell scripts

--- eks-vpc/install.sh
+++
@@ -1,2 +0,0 @@
-helm upgrade --install --namespace kube-system aws-vpc-cni eks/aws-vpc-cni \
-         --set serviceAccount.annotations."eks\.amazonaws\.com/role-arn"=arn:aws:iam::824331038818:role/plt-or-test-k8s-role-aws-nodeau5b
