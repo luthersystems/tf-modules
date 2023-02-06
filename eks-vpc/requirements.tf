f676ee5d1c8cf9926f5d32556919bb184423ea9b
Sam Wood <sam.wood@luthersystems.com>
2023-02-05 17:15:58 -0800

    Add us-east-1 alias

--- eks-vpc/requirements.tf
+++ eks-vpc/requirements.tf
@@ -4,6 +4,7 @@ terraform {
     aws = {
       source                = "hashicorp/aws"
       version               = ">= 3.0"
+      configuration_aliases = [aws.us-east-1]
     }
     external = {
       source = "hashicorp/external"
