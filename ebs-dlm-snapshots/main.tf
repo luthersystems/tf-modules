0cfbe7933b23596874794dc707dd089aa824c970
Sam Wood <sam.wood@luthersystems.com>
2023-03-17 08:16:36 -0700

    Fix KMS permissions for cross region snapshots

--- ebs-dlm-snapshots/main.tf
+++ ebs-dlm-snapshots/main.tf
@@ -2,29 +2,52 @@ locals {
   default_role_arn   = "arn:aws:iam::967058059066:role/dlm-lifecycle"
   execution_role_arn = var.role_arn == "" ? local.default_role_arn : var.role_arn
   execution_role     = split("/", local.execution_role_arn)[1]
+
+  source_ebs_kms_arns = data.aws_ebs_volume.target_volume.*.kms_key_id
+}
+
+
+data "aws_iam_policy_document" "kms_ebs_dr_snapshots" {
+  for_each = { for s in var.cross_region_settings : s.region => s.cmk_arn if s.cmk_arn != null }
+
+  statement {
+    actions = [
+      "kms:RevokeGrant",
+      "kms:CreateGrant",
+      "kms:ListGrants",
+
+    ]
+
+    condition {
+      test     = "Bool"
+      variable = "kms:GrantIsForAWSResource"
+
+      values = [true]
+    }
+
+    resources = compact(concat(each.value, local.source_ebs_kms_arns))
+  }
+
+  statement {
+    actions = [
+      "kms:Encrypt",
+      "kms:Decrypt",
+      "kms:ReEncrypt*",
+      "kms:GenerateDataKey*",
+      "kms:DescribeKey",
+    ]
+
+    resources = compact(concat(each.value, local.source_ebs_kms_arns))
+  }
 }
 
 resource "aws_iam_policy" "kms_ebs_dr_snapshots" {
-  for_each = { for s in var.cross_region_settings : s.region => s.cmk_arn if s.cmk_arn != null }
+  for_each = data.aws_iam_policy_document.kms_ebs_dr_snapshots
 
   name        = "DestinationKmsEBSSnapshotsPolicy"
   description = "KMS permissions for the destination key in cross-region EBS snapshot replication"
 
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "kms:CreateGrant",
-          "kms:Encrypt",
-          "kms:DescribeKey",
-          "kms:ReEncryptFrom"
-        ]
-        Effect   = "Allow"
-        Resource = each.value
-      }
-    ]
-  })
+  policy = each.value.json
 }
 
 resource "aws_iam_role_policy_attachment" "kms_ebs_dr_snapshots_attach" {
@@ -32,6 +55,21 @@ resource "aws_iam_role_policy_attachment" "kms_ebs_dr_snapshots_attach" {
 
   policy_arn = each.value.arn
   role       = local.execution_role
+}
+
+data "aws_ebs_volumes" "target_volumes" {
+  tags = var.target_tags
+}
+
+data "aws_ebs_volume" "target_volume" {
+  for_each = data.aws_ebs_volumes.target_volumes.ids
+
+  most_recent = true
+
+  filter {
+    name   = "volume-id"
+    values = each.value
+  }
 }
 
 resource "aws_dlm_lifecycle_policy" "policy" {
