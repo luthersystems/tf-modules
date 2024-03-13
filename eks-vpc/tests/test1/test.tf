0e75a663369684d9b91a7d72c3c26887e9810a61
sam-at-luther <sam.wood@luthersystems.com>
2024-03-12 18:55:54 -0700

    Bump aws provider in eks-vpc

--- eks-vpc/tests/test1/test.tf
+++ eks-vpc/tests/test1/test.tf
@@ -3,8 +3,8 @@ data "aws_caller_identity" "current" {}
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "~> 4.66"
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
   }
 }
