2a807d42225905e59922ad53f67f5d1be03ae340
sam-at-luther <sam.wood@luthersystems.com>
2024-04-30 12:35:13 -0700

    Fix platform-ui module

--- aws-platform-ui-main/ansible_facts.tf
+++ aws-platform-ui-main/ansible_facts.tf
@@ -40,9 +40,8 @@ locals {
     fluentbit_service_account_iam_role_arn = module.eks_vpc.k8s_facts.fluentbit_service_account_iam_role_arn
     fluentbit_log_group_name               = module.eks_vpc.k8s_facts.fluentbit_log_group_name
 
-    k8s_fabric_peer_service_account_iam_role_arn    = module.eks_vpc.fabric_peer_service_account_iam_role_arn
-    k8s_fabric_cli_service_account_iam_role_arn     = module.eks_vpc.fabric_cli_service_account_iam_role_arn
-    k8s_fabric_orderer_service_account_iam_role_arn = module.eks_vpc.fabric_orderer_service_account_iam_role_arn
+    k8s_fabric_ro_service_account_iam_role_arns       = module.eks_vpc.fabric_ro_service_account_iam_role_arns
+    k8s_fabric_snapshot_service_account_iam_role_arns = module.eks_vpc.fabric_snapshot_service_account_iam_role_arns
   }
 }
 
