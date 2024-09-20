3679d6f1167ce7cb7e0de28ecacf94d3c4f80166
sam-at-luther <sam.wood@luthersystems.com>
2024-09-19 17:07:15 -0700

    Add missing files

---
+++ aws-cf-reverse-proxy/lambda.tf
@@ -0,0 +1,74 @@
+module "luthername_redirect_lambda_role" {
+  count = var.use_302 ? 1 : 0
+
+  source         = "../luthername"
+  luther_project = var.luther_project
+  aws_region     = var.aws_region
+  luther_env     = var.luther_env
+  component      = "lambda"
+  subcomponent   = "redirect"
+  resource       = "role"
+}
+
+resource "aws_iam_role" "lambda_edge_role" {
+  count = var.use_302 ? 1 : 0
+
+  name = module.luthername_redirect_lambda_role[0].name
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_edge_policy" {
+  count = var.use_302 ? 1 : 0
+
+  role       = aws_iam_role.lambda_edge_role[0].name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+module "luthername_redirect_lambda" {
+  count = var.use_302 ? 1 : 0
+
+  source         = "../luthername"
+  luther_project = var.luther_project
+  aws_region     = var.aws_region
+  luther_env     = var.luther_env
+  component      = "cf"
+  subcomponent   = "redirect"
+  resource       = "lambda"
+}
+
+resource "aws_lambda_function" "edge_function" {
+  count = var.use_302 ? 1 : 0
+
+  function_name = module.luthername_redirect_lambda[0].name
+  role          = aws_iam_role.lambda_edge_role[0].arn
+  handler       = "index.handler"
+  runtime       = "nodejs20.x"
+  publish       = true
+  filename      = "${path.module}/lambda.zip"
+
+  # Environment variables or other config
+  environment {
+    variables = {
+      REDIRECT_URL       = var.origin_url
+      REDIRECT_HTTP_CODE = 302
+    }
+  }
+
+  tags = module.luthername_redirect_lambda[0].tags
+}
+
+data "archive_file" "lambda_edge_zip" {
+  type        = "zip"
+  source_dir  = "${path.module}/src"
+  output_path = "${path.module}/lambda.zip"
+}
---
+++ aws-cf-reverse-proxy/src/index.js
@@ -0,0 +1,33 @@
+"use strict";
+
+exports.handler = async (event) => {
+  const request = event.Records[0].cf.request;
+  console.log("Request received:", JSON.stringify(request));
+
+  // Load the redirect URL and HTTP code from environment variables
+  const redirectUrlBase = process.env.REDIRECT_URL;
+  const redirectHttpCode = process.env.REDIRECT_HTTP_CODE || "302"; // Default to 302 if not set
+
+  // Construct the full redirect URL with the URI and query string
+  const redirectUrl = redirectUrlBase + request.uri;
+  const queryString = request.querystring ? "?" + request.querystring : "";
+
+  const newUrl = redirectUrl + queryString;
+  console.log("Redirecting to:", newUrl, "with status code:", redirectHttpCode);
+
+  // Return the redirect response
+  const response = {
+    status: redirectHttpCode,
+    statusDescription: "Found",
+    headers: {
+      location: [
+        {
+          key: "Location",
+          value: newUrl,
+        },
+      ],
+    },
+  };
+
+  return response;
+};
