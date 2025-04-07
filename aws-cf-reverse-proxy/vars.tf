43aaa8c19659764bc0948d84836024306763527b
sam-at-luther <sam.wood@luthersystems.com>
2025-04-07 15:16:46 -0700

    Make new argument optional

--- aws-cf-reverse-proxy/vars.tf
+++ aws-cf-reverse-proxy/vars.tf
@@ -44,6 +44,7 @@ variable "cors_allowed_origins" {
 variable "app_route53_zone_name" {
   type        = string
   description = "The exact Route53 zone name (e.g., app.luthersystems.com) to use for DNS validation and record creation"
+  default     = ""
 }
 
 variable "app_naked_domain" {
